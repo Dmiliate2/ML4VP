8496b413c87d2b3e4ed74d222eb01847ed0fb4e3
Dmiliate2 <100120272+Dmiliate2@users.noreply.github.com>
2025-12-08 12:07:17 -0800

    Update Patch

--- apply_PyL3dMD_patch.py
+++ apply_PyL3dMD_patch.py
@@ -4,7 +4,7 @@ import pyl3dmd
 
 # small patch 
 src = "patched/pyl3dmd.py"
-dst = os.path.join(os.path.dirname(somepackage.__file__), "pyl3dmd.py")
+dst = os.path.join(os.path.dirname(pyl3dmd.__file__), "pyl3dmd.py")
 
 shutil.copy(src, dst)
 print("Patch applied!")
